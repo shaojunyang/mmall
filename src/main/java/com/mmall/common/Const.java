5bea12998669aaf1223acc1ff8e2416f7f3e84ac
shaojunyang <yang152389@139.com>
2017-12-09 13:32:58 +0800

    提交

--- src/main/java/com/mmall/common/Const.java
+++ src/main/java/com/mmall/common/Const.java
@@ -48,4 +48,117 @@ public class Const {
         }
     }
 
+
+    public interface Cart {
+        int CHECKED = 1;// 购物车选中状态
+        int UN_CHECKED = 0;//购物车未选
+
+        // 限制成功 和 限制失败
+        String LIMIT_NUM_FAIL = "LIMIT_NUM_FAIL";
+        String LIMIT_NUM_SUCCESS = "LIMIT_NUM_SUCCESS";
+
+    }
+
+    public enum OrderStatusEnum {
+        CANCELED(0, "已取消"),
+        NO_PAY(10, "未支付"),
+        PAID(20, "已付款"),
+        SHIPPED(40, "已发货"),
+        ORDER_SUCCESS(50, "订单完成"),
+        ORDER_CLOSE(60, "订单关闭");
+
+        OrderStatusEnum(int code, String value) {
+            this.code = code;
+            this.value = value;
+        }
+
+        private int code;
+        private String value;
+
+
+        public String getValue() {
+            return value;
+        }
+
+        public int getCode() {
+            return code;
+        }
+
+        // 把 对象 code码的 汉字描述返回回去
+        public static OrderStatusEnum codeOf(int code) {
+            for (OrderStatusEnum orderStatusEnum : values()) {
+                if (orderStatusEnum.getCode() == code) {
+                    return orderStatusEnum;
+                }
+            }
+            throw new RuntimeException("没有找到对应的枚举");
+
+        }
+
+    }
+
+    public interface AlipayCallback {
+        String TRADE_STATUS_WAIT_BUYER_PAY = "WAIT_BUYER_PAY";
+        String TRADE_STATUS_TRADE_SUCCESS = "TRADE_SUCCESS";
+
+        String RESPONSE_SUCCESS = "success";
+        String RESPONSE_FAILED = "failed";
+    }
+
+
+    public enum PayPlatformEnum {
+        ALIPAY(1, "支付宝");
+
+        PayPlatformEnum(int code, String value) {
+            this.value = value;
+            this.code = code;
+        }
+
+        private int code;
+        private String value;
+
+
+        public String getValue() {
+            return value;
+        }
+
+        public int getCode() {
+            return code;
+        }
+    }
+
+
+    public enum PaymentTypeEnum {
+        ONLINE_PAY(1, "在线支付");
+
+        private int code;
+        private String value;
+
+        public int getCode() {
+            return code;
+        }
+
+        public String getValue() {
+            return value;
+        }
+
+        PaymentTypeEnum(int code, String value) {
+
+            this.code = code;
+            this.value = value;
+        }
+
+        // 把 对象 code码的 汉字描述返回回去
+        public static PaymentTypeEnum codeOf(int code) {
+            for (PaymentTypeEnum paymentTypeEnum : values()) {
+                if (paymentTypeEnum.getCode() == code) {
+                    return paymentTypeEnum;
+                }
+            }
+            throw new RuntimeException("没有找到对应的枚举");
+
+        }
+    }
+
+
 }
--- src/main/java/com/mmall/dao/ShippingMapper.java
+++ src/main/java/com/mmall/dao/ShippingMapper.java
@@ -1,6 +1,9 @@
 package com.mmall.dao;
 
 import com.mmall.pojo.Shipping;
+import org.apache.ibatis.annotations.Param;
+
+import java.util.List;
 
 public interface ShippingMapper {
     int deleteByPrimaryKey(Integer id);
@@ -14,4 +17,34 @@ public interface ShippingMapper {
     int updateByPrimaryKeySelective(Shipping record);
 
     int updateByPrimaryKey(Shipping record);
+
+    /**
+     * 根据 用户id和 收货地址id 删除收货地址
+     * @param userId
+     * @param shippingId
+     * @return
+     */
+    int deleteByShippingIdUserId(@Param("userId") Integer userId, @Param("shippingId") Integer shippingId);
+
+    /**
+     * 根据 用户id和 收货地址id 更新收货地址
+     * @param shipping
+     * @return
+     */
+    int updateByShipping(Shipping shipping);
+
+    /**
+     * 根据 收货地址id 和 用户id  查询 用户 收货地址
+     * @param userId
+     * @param shippingId
+     * @return
+     */
+     Shipping  selectByShippingUserId(@Param("userId") Integer userId, @Param("shippingId") Integer shippingId);
+
+    /**
+     * 根据用户id 查询收货地址列表
+     * @param userId
+     * @return
+     */
+    List<Shipping> selectByUserId(Integer userId);
 }
