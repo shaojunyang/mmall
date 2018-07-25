e319c1d50f54dd94c994fb90a123af5ab01a7681
shaojunyang <yang152389@139.com>
2018-07-25 11:09:18 +0800

    add test mail 5

--- src/main/java/com/mmall/controller/portal/sms/SMSController.java
+++ src/main/java/com/mmall/controller/portal/sms/SMSController.java
@@ -30,39 +30,50 @@ import java.util.Map;
 @RequestMapping("/sms")
 public class SMSController {
 
+    // 发送邮件对象
     @Autowired
     private PwdMailSender pwdMailSender;
 
 
+    // 留言对象dao
     @Autowired
     LiuyanMapper liuyanMapper;
 
+    /**
+     * 发送验证码信息
+     * @param mobile 手机号
+     * @param callback
+     * @param response
+     * @param request
+     * @return 验证码json字符串
+     * @throws IOException
+     */
     @ResponseBody
     @RequestMapping("/send-msg.do")
     public String send_msg(@RequestParam(value = "mobile") String mobile, @RequestParam(value = "callback") String callback, HttpServletResponse response, HttpServletRequest request) throws IOException {
-//        response.setHeader("Access-Control-Allow-Origin", "*");
-//        response.setHeader("Access-Control-Max-Age", "3600");
-//        response.setHeader("Access-Control-Allow-Credentials", "true");
-//        response.setHeader("Access-Control-Allow-Origin", request.getHeader("Origin"));
-//
+
         String ApiKEY = "82ed39a542435fbeabd2ea47fa53af0f";
         Integer random = createRandom();
         String text = "【云合万家】您的验证码是" + random + "。如非本人操作，请忽略本短信";
+
         String result = JavaSmsApi.sendSms(ApiKEY, text, mobile);
-//
+
         Map<String, String> params = new HashMap<String, String>();
         params.put("result", result);
         params.put("code", random.toString());
-//        return ServerResponse.createBySuccess(params);
 
         String r = "{'code':" + random + "}";
-//    加上返回参数
+        //    加上返回参数
         r = callback + "(" + r + ")";
         return r;
 
     }
 
 
+    /**
+     * 生成4位验证码
+     * @return
+     */
     private Integer createRandom() {
         Integer randomNum = (int) (Math.random() * 9000) + 1000;
         return randomNum;
@@ -83,9 +94,7 @@ public class SMSController {
         if (StringUtils.isEmpty(liuyan.getCompany())) {
             liuyan.setCompany("空");
         }
-
-
-
+        // ajax允许跨域
         openOrigin(response, request);
 
         Integer insert = liuyanMapper.insert(liuyan);
@@ -94,36 +103,49 @@ public class SMSController {
             send_mail(liuyan);
             return ServerResponse.createBySuccessMessage("提交成功");
         }
-        return ServerResponse.createBySuccessMessage("提交失败，请稍后重试");
+        return ServerResponse.createBySuccessMessage("提交失败，请稍后60s重试");
     }
 
     /**
      * 发送邮件
-     * @param liuyan 邮件内容
+     *
+     * @param liuyan 邮件相关信息对象
      */
     private void send_mail(Liuyan liuyan) {
-
-       String content =   dateToString() + liuyan.getName() + "  ,    刚刚 在网站提交了试用申请留言，手机号码是：  "  +  liuyan.getMobile() + "  ,  回复内容为： " + liuyan.getContent() + "  ，请及时联系客户";
-        pwdMailSender.sendMail("yasong_wang@araya.cn", "收到一条网站申请试用留言-请及时处理", content);
+        String toMail = "yasong_wang@araya.cn";
+        String subject = "阿赖雅官网收到一条解决方案申请试用留言-请联系客户";
+        String content = "时间: " + dateToString() + "\n" + "姓名: " + liuyan.getName() + "     刚刚 在网站提交了试用申请留言 。" + "\n" + " 手机号：  " + liuyan.getMobile() + "\n" + "邮箱 :" + liuyan.getEmail() + "\n" + "公司名称：" + liuyan.getCompany() + "\n" + " 回复内容为： " + liuyan.getContent() + "，请及时联系客户";
+        pwdMailSender.sendMail(toMail, subject, content);
 
     }
 
-    public String dateToString(){
- //时间转字符串
-    //截取当前系统时间
-    Date currentTime = new Date();
-    //改变输出格式（自己想要的格式）
-    SimpleDateFormat formatter = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
-    //得到字符串时间
-    String s8 = formatter.format(currentTime);
-            return s8;
+
+    /**
+     * 当前时间转字符串
+     * @return 转换之后的字符串 yyyy-MM-dd HH:mm:ss
+     */
+    public String dateToString() {
+        //截取当前系统时间
+        Date currentTime = new Date();
+        //改变输出格式（自己想要的格式）
+        SimpleDateFormat formatter = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
+        //得到字符串时间
+        String s8 = formatter.format(currentTime);
+        return s8;
     }
 
     @Test
-    public void test1(){
+    public void test1() {
         System.out.println(dateToString());
     }
 
+
+    /**
+     * 查询申请试用留言的列表
+     * @param request
+     * @param response
+     * @return
+     */
     @ResponseBody
     @RequestMapping("/select_liuyan.do")
     public ServerResponse<List<Liuyan>> select_liyan(HttpServletRequest request, HttpServletResponse response) {
@@ -135,7 +157,13 @@ public class SMSController {
     }
 
 
-    private  void  openOrigin(HttpServletResponse response,HttpServletRequest request) {
+    /**
+     * 设置允许跨域请求的方法
+     *
+     * @param response
+     * @param request
+     */
+    private void openOrigin(HttpServletResponse response, HttpServletRequest request) {
 
         response.setHeader("Access-Control-Allow-Origin", "*");
         response.setHeader("Access-Control-Max-Age", "3600");
@@ -158,12 +186,11 @@ public class SMSController {
 
     @ResponseBody
     @RequestMapping("/test.do")
-    public String test(){
+    public String test() {
 
         return "334";
 
     }
-
 
 
 }
